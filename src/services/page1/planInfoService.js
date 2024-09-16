e81a9181c5cd9310d8452308e0644c1b696c182d
IslomKhurramov <islom.khurramov@gmail.com>
2024-09-16 23:05:45 +0900

    feat: changing getAllPlanList API

--- src/services/page1/planInfoService.js
+++ src/services/page1/planInfoService.js
@@ -3,7 +3,7 @@ import { serverApi } from '../../lib/config';
 
 export const getAllPlanInfo = async () => {
   try {
-    const response = await axios.get(`https://119.65.247.158:9001/api/getAllPlanInfo/`);
+    const response = await axios.get(`https://119.65.247.158:9001/api/getAllPlanLists/`);
     return response.data;
   } catch (error) {
     console.error('Error fetching plan info:', error);

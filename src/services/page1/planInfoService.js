135bed027e439bedb86b65d7268841a6486873b5
jonik2909 <boronovjonibek@gmail.com>
2024-10-25 15:35:46 +0900

    fix: modify design

--- src/services/page1/planInfoService.js
+++ src/services/page1/planInfoService.js
@@ -3,7 +3,11 @@ import { serverApi } from "../../lib/config";
 
 export const getAllPlanLists = async () => {
   try {
-    const response = await axios.get(`${serverApi}/api/getAllPlanLists/`);
+    const response = await axios.get(`${serverApi}/api/getAllPlanLists/`, {
+      withCredentials: true,
+    });
+
+    console.log("getAllPlanLists RESPONSE:", response);
     return response.data;
   } catch (error) {
     console.error("Error fetching plan info:", error);
